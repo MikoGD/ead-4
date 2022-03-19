77a4a3e46f496ceed167b7ee9cc7baf0344fe85e
Mikael Escolin <mikaelescolin@gmail.com>
2022-03-19 14:04:20 +0000

    Remove utils as it is not needed anymore
    - Only util function moved to colors.ts

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function loadColorsJSON() {
-  const colorsPath = path.resolve(__dirname, process.env.COLORS_PATH ?? '');
-  const colorsFile = fs.readFileSync(colorsPath);
-  return JSON.parse(String(colorsFile));
-}
