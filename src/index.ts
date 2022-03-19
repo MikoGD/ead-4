9b93b014579da004a373e67e28278bc71fd9f1f3
Mikael Escolin <mikaelescolin@gmail.com>
2022-03-19 13:06:26 +0000

    Add first file for basic REST api
    - Add env file for PORT
    - Setup server with morgan and first basic request

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, there');
+import express from 'express';
+import morgan from 'morgan';
+import 'dotenv/config';
+
+const server = express();
+
+server.use(morgan('dev'));
+
+server.get('/', (_, res) => {
+  res.status(200).json({ message: 'ok', data: [] });
+});
+
+server.listen(process.env);
