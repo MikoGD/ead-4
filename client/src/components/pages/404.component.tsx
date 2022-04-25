398ae494568d2f90e4d4df20330e2bcf10c1d50d
Mikael Escolin <mikaelescolin@gmail.com>
2022-04-25 16:12:22 +0100

    Add 404 page

---
+++ client/src/components/pages/404.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ErrorPage() {
+  return (
+    <div className="w-75 mx-auto bg-white d-flex flex-column justify-content-center align-items-center">
+      <h1>Error 404</h1>
+      <h3>Page not found</h3>
+    </div>
+  );
+}
--- client/src/components/pages/index.ts
+++ client/src/components/pages/index.ts
@@ -3,5 +3,13 @@ import AddPage from './add.component';
 import UpdatePage from './update.component';
 import RemovePage from './remove.component';
 import BackroundColorPage from './background-color.component';
+import ErrorPage from './404.component';
 
-export { DisplayPage, AddPage, UpdatePage, RemovePage, BackroundColorPage };
+export {
+  DisplayPage,
+  AddPage,
+  UpdatePage,
+  RemovePage,
+  BackroundColorPage,
+  ErrorPage,
+};
